198bdb0082d36c51d15ff4b7946a7e2fdb37f67a
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-07-26 16:00:32 +0900

    プロジェクト作成時の状態を格納

---
+++ src/test/java/jp/co/cybermissions/itspj/java/empmanager/EmpManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.cybermissions.itspj.java.empmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
