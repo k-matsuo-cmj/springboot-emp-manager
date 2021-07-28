3305002bb32ef7aa34e462ad63174df1faaa0b1f
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-07-28 15:32:21 +0900

    従業員詳細画面を追加

--- src/main/java/jp/co/cybermissions/itspj/java/empmanager/controller/EmployeeController.java
+++ src/main/java/jp/co/cybermissions/itspj/java/empmanager/controller/EmployeeController.java
@@ -3,8 +3,10 @@ package jp.co.cybermissions.itspj.java.empmanager.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.cybermissions.itspj.java.empmanager.model.Employee;
 import jp.co.cybermissions.itspj.java.empmanager.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -22,5 +24,12 @@ public class EmployeeController {
   public String getList(Model model) {
     model.addAttribute("empList", rep.findAll());
     return "emp/list";
+  }
+
+  @GetMapping("/{id}")
+  public String getDetail(@PathVariable int id, Model model) {
+    Employee emp = rep.findById(id).get();
+    model.addAttribute("emp", emp);
+    return "emp/detail";
   }
 }
