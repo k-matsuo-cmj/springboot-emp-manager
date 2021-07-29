e8a5803edfffe69b0ae94112775507a12686252d
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-07-29 10:51:08 +0900

    ログインしている従業員の情報を取得できるようにする

--- src/main/java/jp/co/cybermissions/itspj/java/empmanager/service/impl/ProfileServiceImpl.java
+++ src/main/java/jp/co/cybermissions/itspj/java/empmanager/service/impl/ProfileServiceImpl.java
@@ -1,19 +1,34 @@
 package jp.co.cybermissions.itspj.java.empmanager.service.impl;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import jp.co.cybermissions.itspj.java.empmanager.model.Employee;
+import jp.co.cybermissions.itspj.java.empmanager.repository.EmployeeRepository;
 import jp.co.cybermissions.itspj.java.empmanager.service.ProfileService;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class ProfileServiceImpl implements ProfileService {
+
+  private final EmployeeRepository rep;
 
   @Override
   public Employee getLoginProfile() {
     // ログイン情報を取得する
+    String loginId = getLoginId();
     // 従業員情報を取得する
-    Employee emp = new Employee(); // TODO
+    Employee emp = rep.findByLoginId(loginId);
     return emp;
   }
 
+  /** 現在ログイン中のログインIDを取得する */
+  private String getLoginId() {
+    // UserDetails.nameにloginIdが格納されている
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    String loginId = auth.getName();
+    return loginId;
+  }
 }
